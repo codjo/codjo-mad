353d56d7c0e47eb5943755d600d91d526f6815e4
CREGO <externe.sebastien.crego@allianzgi.fr>
2012-04-25 16:47:27 +0200

    Internationalization of requetor message in the RequesToolbar.

--- codjo-mad-gui/src/test/java/net/codjo/mad/gui/base/ButtonBuilderTest.java
+++ codjo-mad-gui/src/test/java/net/codjo/mad/gui/base/ButtonBuilderTest.java
@@ -1,7 +1,14 @@
 package net.codjo.mad.gui.base;
+import java.util.ListResourceBundle;
 import javax.swing.JButton;
-import static org.junit.Assert.assertEquals;
+import net.codjo.i18n.common.Language;
+import net.codjo.i18n.common.TranslationManager;
+import net.codjo.i18n.gui.TranslationNotifier;
+import net.codjo.mad.gui.i18n.InternationalizationUtil;
+import net.codjo.mad.gui.util.InternationalizableGuiContext;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 /**
  *
  */
@@ -12,5 +19,55 @@ public class ButtonBuilderTest {
         JButton button = new ButtonBuilder("name of the button", new ActionMock()).build();
 
         assertEquals("name of the button", button.getName());
+    }
+
+
+    @Test
+    public void test_buildInternationalizableButton() throws Exception {
+        InternationalizableGuiContext guiContext = new InternationalizableGuiContext();
+        TranslationManager translationManager = InternationalizationUtil.retrieveTranslationManager(guiContext);
+        translationManager.addBundle(new MyFrenchResources(), Language.FR);
+        translationManager.addBundle(new MyEnglishResources(), Language.EN);
+        TranslationNotifier translationNotifier = InternationalizationUtil.retrieveTranslationNotifier(guiContext);
+
+        JButton button =
+              new ButtonBuilder(guiContext, "name of the button", new ActionMock(), "test.label", "test.tooltip")
+                    .build();
+
+        assertEquals("name of the button", button.getName());
+        assertEquals("mon label", button.getText());
+        assertEquals("mon infobulle", button.getToolTipText());
+
+        translationNotifier.setLanguage(Language.EN);
+
+        assertEquals("my label", button.getText());
+        assertEquals("my tooltip", button.getToolTipText());
+    }
+
+
+    private static class MyFrenchResources extends ListResourceBundle {
+        private static final Object[][] CONTENTS = new Object[][]{
+              {"test.label", "mon label"},
+              {"test.tooltip", "mon infobulle"},
+        };
+
+
+        @Override
+        public Object[][] getContents() {
+            return CONTENTS;
+        }
+    }
+
+    private static class MyEnglishResources extends ListResourceBundle {
+        private static final Object[][] CONTENTS = new Object[][]{
+              {"test.label", "my label"},
+              {"test.tooltip", "my tooltip"},
+        };
+
+
+        @Override
+        public Object[][] getContents() {
+            return CONTENTS;
+        }
     }
 }
